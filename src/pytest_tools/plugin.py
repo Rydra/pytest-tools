954f1aeb4660362bf237cf43716e5403025c09b2
David <davigetto@gmail.com>
2021-07-12 15:12:46 +0200

    Add fallback to the shared_datadir fixture if the default one doesn't exist

--- src/pytest_tools/plugin.py
+++ src/pytest_tools/plugin.py
@@ -2,11 +2,14 @@ import pytest
 
 
 @pytest.fixture
-def request_loader(datadir):
+def request_loader(shared_datadir, datadir):
     def get_filecontents(request_filename):
         if '.graphql' not in request_filename:
             request_filename = request_filename + '.graphql'
 
-        return (datadir / f'requests/{request_filename}').read_text()
+        try:
+            return (datadir / f'requests/{request_filename}').read_text()
+        except FileNotFoundError:
+            return (shared_datadir / f'requests/{request_filename}').read_text()
 
     return get_filecontents
