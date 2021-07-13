189eaab823d308c315043af8a5ffc9600a620237
David <davigetto@gmail.com>
2021-07-13 11:44:01 +0200

    Split the request loader between the shared and non-shared versions

--- src/pytest_tools/plugin.py
+++ src/pytest_tools/plugin.py
@@ -2,14 +2,22 @@ import pytest
 
 
 @pytest.fixture
-def request_loader(shared_datadir, datadir):
+def request_loader(datadir):
     def get_filecontents(request_filename):
         if '.graphql' not in request_filename:
             request_filename = request_filename + '.graphql'
 
-        try:
-            return (datadir / f'requests/{request_filename}').read_text()
-        except FileNotFoundError:
-            return (shared_datadir / f'requests/{request_filename}').read_text()
+        return (datadir / f'requests/{request_filename}').read_text()
+
+    return get_filecontents
+
+
+@pytest.fixture
+def shared_request_loader(shared_datadir):
+    def get_filecontents(request_filename):
+        if '.graphql' not in request_filename:
+            request_filename = request_filename + '.graphql'
+
+        return (shared_datadir / f'requests/{request_filename}').read_text()
 
     return get_filecontents
